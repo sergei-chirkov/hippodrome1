d2ba9cf52ce0c071ac74fd3b76f9d0dd3c8db8a5
PK <sergei-chircov@mail.ru>
2023-08-28 20:12:47 +0300

    log4j2.properties for File

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger();
+
 
     public static void main(String[] args) throws Exception {
         List<Horse> horses = List.of(
@@ -15,6 +22,11 @@ public class Main {
         );
         Hippodrome hippodrome = new Hippodrome(horses);
 
+
+        logger.info( " Начало скачек. Количество участников: " + horses.size() );
+
+
+
         for (int i = 0; i < 100; i++) {
             hippodrome.move();
             watch(hippodrome);
@@ -23,6 +35,7 @@ public class Main {
 
         String winnerName = hippodrome.getWinner().getName();
         System.out.println("Победил " + winnerName + "!");
+        logger.info( "Окончание скачек. Победитель: " + winnerName );
     }
 
     private static void watch(Hippodrome hippodrome) throws Exception {
